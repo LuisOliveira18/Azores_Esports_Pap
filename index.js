44f28741c439a986692f1e8765c058d3a8e7934e
Luis0liveira18 <lfrtofficial@hotmail.com>
2022-02-03 22:52:20 +0000

    AZAULA/04

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const path = require('path');
-const path = require('mysql2');
+const mysql2 = require('mysql2');
 const app = express()
 
-
+/*
 const db = mysql2.createConnection({
     host: 'localhost',
     user: 'root',
     password : 'root',
     database: 'dbPAP',
 })
-
+*/
 app.use(express.static('./public'))
 
 app.get('/',(req,res)=> {
-  res.sendFile(path.join(__dirname, './public/azpi.html'))
+  res.sendFile(path.join(__dirname, 'public/html/azpi.html'))
 })
 const port = 3001
 

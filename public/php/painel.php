af748a64c2cd57d3a21721bf4f3535544dd364ed
Luis Oliveira <lfrtofficial@hotmail.com>
2022-03-11 23:37:53 +0000

    jhhh

--- public/php/painel.php
+++
@@ -1,6 +0,0 @@
-<?php
-section_start();
-?>
-
-<h2>Teste<?php echo $_SESSION['utilizador'];?><h2>
-<h2><a href="logout.php">Sair</a></h2>
--- public/php/verifica_login.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if (!$_SESSION['utilizador']) {
-    header('location: index.php');
-    exit();
-}
